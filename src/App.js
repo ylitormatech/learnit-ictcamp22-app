be654992123ece06c9610212ab9206c395a7d24a
hongatar <henrietta.kontio@gmail.com>
2022-12-19 14:02:23 +0200

    >

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ Amplify.configure(awsExports);
 function App({ signOut, user }) {
   return (
       <>
-<<<<<<< HEAD
         <div class='header'>
           <h1>BEARIT</h1>
         </div>
@@ -26,19 +25,9 @@ function App({ signOut, user }) {
         <p>Hello {user.username}</p>
         <button onClick={signOut}>Kirjaudu ulos</button>
         <NavigationButtonForward />
-=======
-        <h1>Vaikuttavuuskysely</h1>
-        <h1>Hello {user.username}</h1>
-        <button onClick={signOut}>Sign out</button>
-        
->>>>>>> e3d0c8f6f31c72d69653456acd98bf35b3d250b9
       </>
   );
 }
 
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e3d0c8f6f31c72d69653456acd98bf35b3d250b9
 export default withAuthenticator(App);
