0582d9e3c62db2a4e4b897ba962cc7bac081a23c
hongatar <henrietta.kontio@gmail.com>
2022-12-19 12:19:51 +0200

    testi

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App({ signOut, user }) {
         <h2>ICT-Camp vaikuttavuuskysely</h2>
         <div class='info'>
           <p>Kyselyn pohjustusteksti. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-          Sed ac ullamcorper ipsum.. Ut sollicitudin justo ut nulla facilisis, egestas congue ex faucibus.</p>
+          Sed ac ullamcorper ipsum... Ut sollicitudin justo ut nulla facilisis, egestas congue ex faucibus.</p>
         </div>
         <p>Hello {user.username}</p>
         <button onClick={signOut}>Kirjaudu ulos</button>
