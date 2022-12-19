ee87a31561c8b0a3ab52c3f58f4bc14a47cdc7b6
DeaAlexandra <alexandra.saarelainen@elisanet.fi>
2022-12-19 18:58:57 +0200

    Viimeisin testaus

--- src/models/index.js
+++ src/models/index.js
@@ -4,17 +4,11 @@ import { schema } from './schema';
 
 
 
-<<<<<<< HEAD
-const { UserProfile, Questions } = initSchema(schema);
-
-export {
-=======
 const { UserAnswers, Selection, UserProfile, Questions } = initSchema(schema);
 
 export {
   UserAnswers,
   Selection,
->>>>>>> 0582d9e3c62db2a4e4b897ba962cc7bac081a23c
   UserProfile,
   Questions
 };
